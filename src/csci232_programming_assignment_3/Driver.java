27ee4d6789fa2e1983efc4fa0c12d790dd0c50b8
Rusty Clayton <rusty_clayton@yahoo.com>
2018-04-05 08:45:01 -0600

    Adding the project initially
    Empty java program to start working on together.

---
+++ src/csci232_programming_assignment_3/Driver.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci232_programming_assignment_3;
+
+/**
+ *
+ * @author Rusty Clayton, Kamran Hetke, Derek Jacobson
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
